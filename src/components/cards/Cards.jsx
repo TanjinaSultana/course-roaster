8ac8870070670b02456bda3aca69a039e85e106f
Tanjina sultana <tanjinasultana388@gmail.com>
2023-09-15 16:18:20 +0600

    json data updated and the cards css also updated..json data changed according to its cover picture

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -8,16 +8,16 @@ const Cards = ({course}) => {
     return (
         <div className="">
             <div className="card w-[312px] h-[400px] shadow-xl">
-  <figure><img className="w-[280px] h-[144px] rounded-xl" src={cover} alt={`${title} no ${id}`} /></figure>
-  <div className="card-body">
-    <h2 className="card-title">{title}</h2>
-    <p>{description}</p>
-    <div className="grid grid-cols-2 gap-4">
-        <p>$ Price : {price}</p>
-        <p>Credit : {credit} hr</p>
+  <figure><img className="w-[280px] h-[144px] rounded-xl mb-0" src={cover} alt={`${title} no ${id}`} /></figure>
+  <div className="  m-4">
+    <h2 className="card-title text-lg font-semibold  ">{title}</h2>
+    <p className="font-normal text-sm mt-2">{description}</p>
+    <div className="grid grid-cols-2 gap-4 mt-4">
+        <p className="text-base font-normal">$ Price : {price}</p>
+        <p className="text-base font-normal">Credit : {credit} hr</p>
     </div>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Select</button>
+    <div className="card-actions justify-center mt-8 ">
+      <button className="btn btn-primary px-[120px]">Select</button>
     </div>
   </div>
 </div>
